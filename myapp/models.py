2769ef9ac9258596a84a3d0e568dea31e081f5f5
nabeelahmdd <nabeelahmddd@gmail.com>
2020-04-03 15:46:38 +0100

    config

--- myapp/models.py
+++ myapp/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+
+
 # Create your models here.
+
 class Notice(models.Model):
     subject = models.CharField(max_length=100)
     mmsg = models.TextField()
     cr_date = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
+
         return self.subject
--- myapp/urls.py
+++ myapp/urls.py
@@ -1,11 +1,22 @@
+
 from django.urls import path
+
 from myapp import views
 
 
 
 
+
+
+
+
+
 urlpatterns = [
+
     path('home/', views.home),
+
     path('notice/', views.NoticeListView.as_view()),
+
+
 
 ]
